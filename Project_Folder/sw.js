8ae626e17476889a874ac5ad6b245d29b1c2557a
abhijitdeogirkar <70119313+abhijitdeogirkar@users.noreply.github.com>
2026-07-11 20:50:47 +0530

    Implement service worker for caching resources

--- Project_Folder/sw.js
+++ Project_Folder/sw.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = 'activity-tracker-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/app.js',
+  './js/camera.js'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
